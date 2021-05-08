c3695f14e962a1604cf1d0f66c125d2f34972cff
thenatural86 <philip.smalls86@gmail.com>
2021-05-08 11:26:12 -0400

    code refactor

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,8 +2,10 @@ import { useState } from 'react'
 
 const SimpleInput = ({ renderForm }) => {
   const [enteredName, setEnteredName] = useState('')
-  const [isValid, setIsValid] = useState(false)
   const [touched, setTouched] = useState(false)
+
+  const isValid = enteredName.trim() !== ''
+  const invalidInput = !isValid && touched
 
   const nameInputHandler = (e) => {
     setEnteredName(e.target.value)
@@ -11,25 +13,20 @@ const SimpleInput = ({ renderForm }) => {
 
   const inputBlurHandler = () => {
     setTouched(true)
-    if (enteredName.length === 0) {
-      setIsValid(false)
-      return
-    }
   }
 
   const formSubmitHandler = (e) => {
     e.preventDefault()
     setTouched(true)
-    if (enteredName.length === 0) {
-      setIsValid(false)
+
+    if (!isValid) {
       return
     }
-    setIsValid(true)
+
     renderForm(enteredName)
     setEnteredName('')
+    setTouched(false)
   }
-
-  const invalidInput = !isValid && touched
 
   const nameInputClass = invalidInput ? 'form-control invalid' : 'form-control'
 
@@ -44,7 +41,7 @@ const SimpleInput = ({ renderForm }) => {
           onChange={nameInputHandler}
           onBlur={inputBlurHandler}
         />
-        {isValid && <p className='error-text'>Name cannot be empty</p>}
+        {invalidInput && <p className='error-text'>Name cannot be empty</p>}
       </div>
       <div className='form-actions'>
         <button onSubmit={formSubmitHandler}>Submit</button>
